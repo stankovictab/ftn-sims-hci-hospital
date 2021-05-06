799c61548114db6ee733ac576a33235f17cd1ce2
zixazmija99 <zivkovic.mihajlo0809@gmail.com>
2021-05-06 11:44:20 +0200

    Final version of Secretary-Master merge

--- HolidayRequestCreation.xaml.cs
+++ HolidayRequestCreation.xaml.cs
@@ -22,21 +22,6 @@ namespace ftn_sims_hci_hospital
             // TODO: Ovde ce se ubacivati id lekara koji je ulogovan
             Classes.Doctor doctor = dc.GetByID("0501");
 
-            // Da bih saznao koji je poslednji id holidayRequest-ova da bih ovom inkrementovao, trebam da prolazim kroz sve i da cim nadje null da mi kaze koji je poslednji
-            string currentID = "1"; // Mora da se postavi jer vristi
-            for (int i = 1; i < 100; i++)
-            {
-                if (hrfs.GetByID(i.ToString()) == null)
-                {
-                    break;
-                }
-                currentID = (i + 1).ToString(); // Ok je ako je prazan string, odnosno ako nema nista u holidayrequests.txt
-            }
-
-            Classes.DoctorFileStorage dfs = new Classes.DoctorFileStorage();
-            dfs.DoctorsInFile1 = dfs.GetAll();
-            Classes.Doctor doctor = dfs.GetByID("0501"); // TODO: Ovde ce se ubacivati id lekara koji je ulogovan
-
             MainWindow.holidayRequestController.Create(desc, startDate, endDate, doctor); // Update-uje se i lista i fajl
             MessageBox.Show("You have successfully created a new holiday request!");
             this.Close(); // this.Hide(); ?
