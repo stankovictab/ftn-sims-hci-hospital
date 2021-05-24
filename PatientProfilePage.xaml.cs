e62663d61ee9a01db76ab308fbcbdb5c25c6f79f
Goran Tatomirov <tatomirov.in12.2018@uns.ac.rs>
2021-05-24 22:22:02 +0200

    podsetnici

---
+++ PatientProfilePage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using ftn_sims_hci_hospital.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ftn_sims_hci_hospital
+{
+    public partial class PatientProfilePage : Page
+    {
+        private ReminderController reminderController;
+        public PatientProfilePage()
+        {
+            reminderController = new ReminderController();
+            InitializeComponent();
+            LoadReminders();
+        }
+
+        private void LoadReminders()
+        {
+            List<Reminder> notHighlightedReminders = reminderController.GetAllByPatientID(MainWindow.user.Jmbg1, false);
+            foreach(Reminder r in notHighlightedReminders)
+            {
+                if(r.Date < DateTime.Now)
+                {
+                    r.Highlighted = true;
+                    reminderController.Update(r);
+                }
+            }
+            List<Reminder> highlightedReminders = setHighlightedToSeen();
+            highlightedRemindersTable.ItemsSource = highlightedReminders;
+            remindersTable.ItemsSource = reminderController.GetAllByPatientID(MainWindow.user.Jmbg1, false);
+        }
+        private List<Reminder> setHighlightedToSeen()
+        {
+            List<Reminder> reminders = reminderController.GetAllByPatientID(MainWindow.user.Jmbg1, true);
+            foreach (Reminder r in reminders)
+            {
+                if (r.Seen)
+                {
+                    reminderController.Delete(r.Id);
+                }
+                else
+                {
+                    r.Seen = true;
+                    reminderController.Delete(r.Id);
+                }
+            }
+            return reminders;
+        }
+
+        private void CancelReminder(object render, RoutedEventArgs e)
+        {
+            Reminder selected = (Reminder)(remindersTable.SelectedItem);
+            reminderController.Delete(selected.Id);
+            LoadReminders();
+        }
+
+        private void UpdateReminder(object render, RoutedEventArgs e)
+        {
+            Reminder selected = (Reminder)(remindersTable.SelectedItem);
+            CreateReminder cr = new CreateReminder(selected);
+            cr.Show();
+        }
+
+        private void CreateReminder(object sender, RoutedEventArgs e)
+        {
+            CreateReminder cr = new CreateReminder(null);
+            cr.Show();
+        }
+    }
+}
--- PatientWindow.xaml.cs
+++ PatientWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Classes;
+using ftn_sims_hci_hospital.Classes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,16 +13,28 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ftn_sims_hci_hospital
 {
     public partial class PatientWindow : Window
     {
-
+        private ReminderController reminderController;
         public PatientWindow()
         {
+            reminderController = new ReminderController();
             InitializeComponent();
             pregledProfilaButton.Content = "Pregled profila " + MainWindow.user.Username1;
+            Main.Content = new PatientProfilePage();
+            InitTimer();
+        }
+
+        private void InitTimer()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += TimerTick;
+            timer.Interval = new TimeSpan(0, 0, 10);
+            timer.Start();
         }
 
         private void ShowAllAppointments(object sender, RoutedEventArgs e)
@@ -42,7 +55,6 @@ namespace ftn_sims_hci_hospital
         private void NotificationsList(object sender, RoutedEventArgs e)
         {
             Main.Content = new NotificationsPage();
-
         }
 
         private void CreateAppointmentSpecialistButton(object sender, RoutedEventArgs e)
@@ -57,13 +69,47 @@ namespace ftn_sims_hci_hospital
 
         private void ViewAccount(object sender, RoutedEventArgs e)
         {
-            Main.Content = null; //TO DO - pregled profila
+            reminderCounter.Content = "0";
+            reminderCounter.Visibility = Visibility.Hidden;
+            reminderCounterBorder.Visibility = Visibility.Hidden;
+            Main.Content = new PatientProfilePage(); 
         }
 
         private void LogOut(object sender, RoutedEventArgs e)
         {
             MainWindow.user = null;
             this.Close();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            List<Reminder> reminders = reminderController.GetAllByPatientID(MainWindow.user.Jmbg1, false);
+            foreach (Reminder r in reminders)
+            {
+                if(r.Date.Year.Equals(DateTime.Now.Year) && r.Date.Month.Equals(DateTime.Now.Month) && r.Date.Day.Equals(DateTime.Now.Day) && r.Date.Hour.Equals(DateTime.Now.Hour) && r.Date.Minute.Equals(DateTime.Now.Minute))
+                {
+                    r.Highlighted = true;
+                    reminderController.Update(r);
+                    UpdateReminderCounter();
+                    if (r.FrequencyInHours > 0)
+                    {
+                        r.Highlighted = false;
+                        r.Date = r.Date.AddHours(r.FrequencyInHours);
+                        reminderController.Create(r);
+                    }
+                }
+            }
+        }
+
+        private void UpdateReminderCounter()
+        {
+            if(reminderCounterBorder.Visibility == Visibility.Hidden)
+            {
+                reminderCounterBorder.Visibility = Visibility.Visible;
+                reminderCounter.Visibility = Visibility.Visible;
+            }
+            int broj = int.Parse(reminderCounter.Content.ToString());
+            reminderCounter.Content = (++broj).ToString();
         }
     }
 }
