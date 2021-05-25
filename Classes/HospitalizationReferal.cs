c2cd15fee6e427734ea3e116e32c5cb37948b736
zixazmija99 <zivkovic.mihajlo0809@gmail.com>
2021-05-25 13:26:48 +0200

    Hospitalization Referral creation

---
+++ Classes/HospitalizationReferal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classes;
+
+namespace ftn_sims_hci_hospital.Classes
+{
+    class HospitalizationReferal
+    {
+        public String id { get; set; }
+        public Patient patient { get; set; }
+        public String description { get; set; }
+        public Room room { get; set; }
+        public DateTime endDate { get; set; }
+        public DateTime startDate { get; set; }
+
+        public HospitalizationReferal(string id, Patient patient, string description, DateTime endDate, DateTime startDate)
+        {
+            this.id = id;
+            this.patient = patient;
+            this.description = description;
+            this.endDate = endDate;
+            this.startDate = startDate;
+        }
+    }
+}
---
+++ Classes/HospitalizationReferalRepository.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classes;
+namespace ftn_sims_hci_hospital.Classes
+{
+    class HospitalizationReferalRepository
+    {
+        private String FileLocation;
+        private PatientRepository patientRepository = new PatientRepository();
+        public HospitalizationReferalRepository()
+        {
+            FileLocation = "../../Text Files/hospitalizationreferals.txt";
+        }
+
+        public Boolean Create(HospitalizationReferal hr)
+        {
+            string newLine = hr.id + ";" + hr.patient.user.Jmbg1 + ";" + hr.description + ";" + hr.startDate.Year.ToString() + "," + hr.startDate.Month.ToString() + "," + hr.startDate.Day.ToString()
+               + "," + hr.startDate.Hour.ToString() + "," + hr.startDate.Minute.ToString() + "," + hr.startDate.Second.ToString()
+               + ";" + hr.endDate.Year.ToString() + "," + hr.endDate.Month.ToString() + "," + hr.endDate.Day.ToString()
+               + "," + hr.endDate.Hour.ToString() + "," + hr.endDate.Minute.ToString() + "," + hr.endDate.Second.ToString() + ";"+hr.room.RoomNumber +"\n";
+            System.IO.File.AppendAllText(FileLocation, newLine);
+            return true;
+        }
+
+        public HospitalizationReferal GetById(String HrefId)
+        {
+            HospitalizationReferal hospitalizationReferral;
+            String[] rows = System.IO.File.ReadAllLines(FileLocation);
+
+            foreach (String row in rows)
+            {
+                String[] data = row.Split(';');
+                if (data[0].Equals(HrefId))
+                {
+                    String id = data[0];
+                    String patientId = data[1];
+                    Patient patient = patientRepository.GetByID(patientId);
+                    String description = data[2];
+                    String[] startDateParts = data[3].Split(',');
+                    DateTime startDate = new DateTime(int.Parse(startDateParts[0]), int.Parse(startDateParts[1]), int.Parse(startDateParts[2]), int.Parse(startDateParts[3]), int.Parse(startDateParts[4]), int.Parse(startDateParts[5]));
+                    String[] endDateParts = data[4].Split(',');
+                    DateTime endDate = new DateTime(int.Parse(startDateParts[0]), int.Parse(startDateParts[1]), int.Parse(startDateParts[2]), int.Parse(startDateParts[3]), int.Parse(startDateParts[4]), int.Parse(startDateParts[5]));
+                    hospitalizationReferral = new HospitalizationReferal(id, patient, description, startDate, endDate);
+                    return hospitalizationReferral;
+                }
+            }
+            return null;
+        }
+
+        public List<HospitalizationReferal> GetAllByPatientId(String patientID)
+        {
+            List<HospitalizationReferal> referrals = new List<HospitalizationReferal>();
+            String[] rows = System.IO.File.ReadAllLines(FileLocation);
+
+            foreach (String row in rows)
+            {
+                String[] data = row.Split(';');
+                if (data[1].Equals(patientID))
+                {
+                    String id = data[0];
+                    String patientId = data[1];
+                    Patient patient = patientRepository.GetByID(patientId);
+                    String description = data[2];
+                    String[] startDateParts = data[3].Split(',');
+                    DateTime startDate = new DateTime(int.Parse(startDateParts[0]), int.Parse(startDateParts[1]), int.Parse(startDateParts[2]), int.Parse(startDateParts[3]), int.Parse(startDateParts[4]), int.Parse(startDateParts[5]));
+                    String[] endDateParts = data[4].Split(',');
+                    DateTime endDate = new DateTime(int.Parse(startDateParts[0]), int.Parse(startDateParts[1]), int.Parse(startDateParts[2]), int.Parse(startDateParts[3]), int.Parse(startDateParts[4]), int.Parse(startDateParts[5]));
+                    HospitalizationReferal hospitalizationReferral = new HospitalizationReferal(id, patient, description, startDate, endDate);
+                    referrals.Add(hospitalizationReferral);
+                }
+            }
+            return referrals;
+        }
+        public Boolean Delete(String id)
+        {
+            String[] rows = System.IO.File.ReadAllLines(FileLocation);
+            List<HospitalizationReferal> referrals = new List<HospitalizationReferal>();
+            List<String> novi = new List<string>();
+            foreach (String row in rows)
+            {
+                String[] data = row.Split(';');
+                if (!data[0].Equals(id))
+                {
+                    String r = String.Join(";", data);
+                    novi.Add(r);
+                }
+            }
+            System.IO.File.WriteAllLines(FileLocation, novi);
+            return true;
+        }
+
+        public Boolean Update(HospitalizationReferal hr)
+        {
+            if (Delete(hr.id))
+            {
+                Create(hr);
+                return true;
+            }
+
+            return false;
+        }
+        public List<HospitalizationReferal> getAllByEndDate(DateTime requestedEndDate)
+        {
+            List<HospitalizationReferal> referralsByEndDate = new List<HospitalizationReferal>();
+            String[] rows = System.IO.File.ReadAllLines(FileLocation);
+            foreach (String row in rows)
+            {
+                String[] data = row.Split(';');
+                String[] endDateParts = data[4].Split(',');
+                if (endDateParts[0].Equals(requestedEndDate.Year.ToString())&&endDateParts[1].Equals(requestedEndDate.Month.ToString())&&endDateParts[2].Equals(requestedEndDate.Day.ToString()))
+                {
+                    String id = data[0];
+                    String patientId = data[1];
+                    Patient patient = patientRepository.GetByID(patientId);
+                    String description = data[2];
+                    String[] startDateParts = data[3].Split(',');
+                    DateTime startDate = new DateTime(int.Parse(startDateParts[0]), int.Parse(startDateParts[1]), int.Parse(startDateParts[2]), int.Parse(startDateParts[3]), int.Parse(startDateParts[4]), int.Parse(startDateParts[5]));
+                    DateTime endDate = new DateTime(int.Parse(startDateParts[0]), int.Parse(startDateParts[1]), int.Parse(startDateParts[2]), int.Parse(startDateParts[3]), int.Parse(startDateParts[4]), int.Parse(startDateParts[5]));
+                    HospitalizationReferal hospitalizationReferral = new HospitalizationReferal(id, patient, description, startDate, endDate);
+                    referralsByEndDate.Add(hospitalizationReferral);
+                }
+            }
+            return referralsByEndDate;
+        }
+    }
+}
+
